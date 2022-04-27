46e402ac1c4bdfc328d88e7dc0d011062aea0d9b
AngeloJS04 <angelo.david499@gmail.com>
2022-04-27 16:33:59 -0400

    interface modal

--- src/interface/Modal.interface.ts
+++ src/interface/Modal.interface.ts
@@ -1,14 +1,21 @@
 import { ReactNode } from "react"
 
 export interface ShowI {
-    active: boolean
-    toggle: Function
-    children: ReactNode
-    header?: any
-    width?: string
-    realWidth?: string
-  }
-  
-  export interface ModalI extends ShowI {
-    header?: string | Function | ReactNode
-  }
+  active: boolean
+  toggle: Function
+  children: ReactNode
+  header?: any
+  width?: string
+  realWidth?: string
+}
+
+export interface ModalI extends ShowI {
+  header?: string | Function | ReactNode
+}
+export interface ModalDataI {
+  id: number,
+  title: string,
+  episodes: string,
+  duration: string
+
+}
